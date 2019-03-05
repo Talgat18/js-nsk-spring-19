65d17d24f1ad37461013d56ab806855eefcead8c
Talgat <taktasimov1@gmail.com>
2019-03-05 23:28:47 +0700

    devServer added

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,13 @@
 const path = require('path');
+//const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
     entry: './typerace/src/js/main.js',
     output: {
-        path: path.resolve(__dirname, './typerace/dist/js'),
-        filename: 'bundle.js'
+        path: path.resolve(__dirname, './typerace/dist'),
+        filename: 'js/bundle.js'
+    },
+    devServer: {
+        contentBase: './typerace/dist'
     }
 };
