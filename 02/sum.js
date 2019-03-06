7beedfdf201c7170ec8aad8b187055f493fd254a
Talgat <taktasimov1@gmail.com>
2019-03-06 14:06:12 +0700

    sum/numberAndString added

--- 02/sum.js
+++ 02/sum.js
@@ -12,4 +12,16 @@
  * @returns а это уже сами решите
  */
 export function sum(x) {
+  if (x === undefined) {
+    return 0;
+  }
+
+  let result = x;
+
+  return function summy(y) {
+    if (y !== undefined) {
+      result += y;
+    }
+    return y === undefined ? result : summy;
+  };
 }
